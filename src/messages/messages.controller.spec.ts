08a3daa82e03fb3bfb68ee18b06055cafda054c8
Kimhanjin <ddang0103@naver.com>
2025-02-27 20:44:34 +0900

    Validaiton Check & Add Valdiation Dependency
    @Body() body: CreateMessageDto -> 타입을 넘겨야하는데 객체를 넘겨도되는이유:  클래스에 속한 각각의 필드에 대한 타입을 넘긴다
    
    스프링의 '@ReqeuestBody 객체' 랑 거의 유사함

--- src/messages/messages.controller.spec.ts
+++ src/messages/messages.controller.spec.ts
@@ -1,13 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MessagesController } from './messages.controller';
 
-describe('MessagesController', () => {
+describe('MessagesController', () => { //describe 은 테스트 그룹을 만드는 함수
   let controller: MessagesController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({  //하단의 it 블록 실행되기전 매번 실행되는 설정 코드
       controllers: [MessagesController],
-    }).compile();
+    }).compile(); //테스트 모듈을 컴파일하여 필요한 의존성을 준비
 
     controller = module.get<MessagesController>(MessagesController);
   });
